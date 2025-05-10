f41e747518c07c52affec3fe2b54c1ad86c3f217
Brandon Popson <brandon@bpopson.com>
2025-05-10 08:12:13 -0400

    Adds MongoDB connection logic and start server asynchronously

--- server.js
+++ server.js
@@ -1,8 +1,35 @@
 import express from "express";
+import { MongoClient, ObjectId } from "mongodb";
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
+const mongoURI = "mongodb://localhost:27017";
+
+const dbName = "usersDB";
+const collectionName = "users";
+
+let db;
+
+const client = new MongoClient(mongoURI);
+
+async function connectToDB() {
+    try {
+        await client.connect();
+        db = client.db(dbName);
+        console.log("Connected to MongoDB");
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+    }
+}
+
+app.use(express.json());
+
+async function startServer() {
+    await connectToDB();
+    app.listen(3000, () => {
+        console.log("Server is running on port 3000");
+    });
+}
+
+startServer();
 
